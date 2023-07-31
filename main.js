eae8cf2b2a0705597cdc3825acbbb1fc5880bac3
adnanfito <adnan.dharmawan@mhs.unsoed.ac.id>
2023-07-31 17:05:49 +0700

    final

---
+++ main.js
@@ -0,0 +1,55 @@
+const checkbox = document.querySelector('#toggle');
+const html = document.querySelector('html');
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+const light = document.querySelector('.light-theme');
+const dark = document.querySelector('.particles');
+const lightProfile = document.querySelector('.light-profile');
+const darkProfile = document.querySelector('.dark-profile');
+
+checkbox.addEventListener('click', function () {
+  if (checkbox.checked) {
+    html.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+    sun.classList.remove('hidden');
+    moon.classList.add('hidden');
+    light.classList.add('hidden');
+    dark.classList.remove('hidden');
+    lightProfile.classList.add('hidden');
+    darkProfile.classList.remove('hidden');
+  } else {
+    html.classList.remove('dark');
+    localStorage.removeItem('theme');
+    moon.classList.remove('hidden');
+    sun.classList.add('hidden');
+    dark.classList.add('hidden');
+    light.classList.remove('hidden');
+    darkProfile.classList.add('hidden');
+    lightProfile.classList.remove('hidden');
+  }
+});
+
+const hamburger = document.querySelector('#hamburger');
+const content = document.querySelector('.hamburger-content');
+
+hamburger.addEventListener('click', function () {
+  if (hamburger.checked) {
+    content.style.right = '0';
+  } else {
+    content.style.right = '-50rem';
+  }
+});
+
+var messageArray = ['Front End Developer'];
+var textPosition = 0;
+var speed = 150;
+
+typewriter = () => {
+  document.querySelector('.status').innerHTML = messageArray[0].substring(0, textPosition) + "<span class='animate-blinker'>\u007C</span>";
+  if (textPosition++ != messageArray[0].length) setTimeout(typewriter, speed);
+  // else {
+  //   textPosition = 0;
+  //   setTimeout(typewriter, speed);
+  // }
+};
+window.addEventListener('load', typewriter);
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'poppins',
+      },
+      animation: {
+        fade: 'fade 0.5s linear',
+        blinker: 'blinker 1s linear infinite',
+      },
+      keyframes: {
+        fade: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        blinker: {
+          '50%': {
+            opacity: 0,
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
